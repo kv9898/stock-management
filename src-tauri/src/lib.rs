9eb197b5e7f06cc9b8b69145227bd05aa45ac30c
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-08-15 10:07:33 +0800

    Add get_all_products to handlers

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,7 @@
 mod db;
+mod product;
+
+use product::get_all_products;
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -10,7 +13,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_all_products])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
