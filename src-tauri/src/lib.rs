b4a46147ad5d17e5c6d6a7779beaff576ce54c1b
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-08-15 14:23:08 +0800

    add delete_product to handle

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,13 @@
 mod db;
 mod product;
 
-use product::get_all_products;
+use product::{delete_product, get_all_products};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_all_products])
+        .invoke_handler(tauri::generate_handler![get_all_products, delete_product])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
