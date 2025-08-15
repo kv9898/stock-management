407f625c276f4c0a5d2977044120ea3d528f0739
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-08-16 00:07:12 +0800

    Move tokens to resources (#13)
    * update tauri.conf.json
    
    * Add OnceCell
    
    * use once cell to store url and token
    
    * Add tauri-fs
    
    * automatic fs code added to lib.rs
    
    * compile exe only

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,17 @@
 mod db;
 mod product;
 
+use db::init_db_tokens;
 use product::{add_product, delete_product, get_all_products, get_product, update_product};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .setup(|app| {
+            init_db_tokens(app)?;
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
             get_all_products,
