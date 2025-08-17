e1ad0a7a16970d0d87a9f874c9157d646c81b9e3
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-08-17 22:18:21 +0800

    Add manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -19,14 +19,33 @@ export default defineConfig(async () => ({
     host: host || false,
     hmr: host
       ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
       : undefined,
     watch: {
       // 3. tell Vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
     },
   },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (/monaco|codemirror/.test(id)) return 'editor'
+            if (/echarts|chart\.js|d3/.test(id)) return 'charts'
+            if (/xlsx|sheetjs/.test(id)) return 'spreadsheets'
+            if (/pdfjs-dist/.test(id)) return 'pdf'
+            if (/three/.test(id)) return 'three'
+            // default vendor split per top-level package
+            const m = id.split('node_modules/')[1].split('/')[0].replace('@', '')
+            return `vendor-${m}`
+          }
+        },
+      },
+    },
+  }
 }));
