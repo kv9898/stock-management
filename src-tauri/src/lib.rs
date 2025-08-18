c824ee69ab63a2d5d0153835ba605ca9071bc428
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-08-18 23:36:56 +0800

    add get_config to invoke handler

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,7 @@ mod product;
 mod stock;
 mod summary;
 
-use config::read_config;
+use config::{get_config, read_config};
 use product::{add_product, delete_product, get_all_products, get_product, update_product};
 use stock::{add_stock, edit_stock, get_in_stock_products, get_stock_lots, remove_stock};
 use summary::{get_stock_histogram, get_stock_overview};
@@ -30,7 +30,8 @@ pub fn run() {
             get_in_stock_products,
             get_stock_lots,
             get_stock_overview,
-            get_stock_histogram
+            get_stock_histogram,
+            get_config,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
