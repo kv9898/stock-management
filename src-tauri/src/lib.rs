5b8ff9300101908f6075a99b2fb8f919a5ce8c17
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-08-19 11:25:20 +0800

    rename read_config to init_config in lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,7 @@ mod product;
 mod stock;
 mod summary;
 
-use config::{get_config, read_config, write_config};
+use config::{get_config, init_config, write_config};
 use product::{add_product, delete_product, get_all_products, get_product, update_product};
 use stock::{add_stock, edit_stock, get_in_stock_products, get_stock_lots, remove_stock};
 use summary::{get_stock_histogram, get_stock_overview};
@@ -14,7 +14,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .setup(|app| {
-            read_config(app)?;
+            init_config(app)?;
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
