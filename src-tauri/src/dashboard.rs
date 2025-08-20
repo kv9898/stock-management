95df1c0659b8742a49bb5a51fb74abcc22617fbb
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-08-20 11:48:18 +0800

    add toy backend

---
+++ src-tauri/src/dashboard.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub total_sellable_value: f32,
+    pub expiring_soon_value: f32,
+    pub expired_value: f32,
+    pub net_loan_value: f32,
+}
+
+#[tauri::command]
+pub fn get_dashboard_summary() -> Result<Config, String> {
+    // Dummy implementation, replace with actual logic to compute values
+    Ok(Config {
+        total_sellable_value: 271658.94,
+        expiring_soon_value: 751254.98,
+        expired_value: 15124.31,
+        net_loan_value: -5214.36,
+    })
+}
