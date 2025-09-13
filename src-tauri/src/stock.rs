7f8f972a47b1a2b2ad826052a1020d85c01ba7a8
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-09-13 14:23:24 +0800

    stock: use add_sale when needed

--- src-tauri/src/stock.rs
+++ src-tauri/src/stock.rs
@@ -4,6 +4,7 @@ use tokio::task;
 use uuid::Uuid;
 
 use crate::db::{ignore_empty_baton_commit, sql_quote};
+use crate::sales::add_sale;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct StockChange {
@@ -141,9 +142,7 @@ pub async fn add_stock(changes: Vec<StockChange>) -> Result<(), String> {
 
 #[tauri::command]
 pub async fn remove_stock(changes: Vec<StockChange>, mark_as_sale: bool) -> Result<(), String> {
-    // TODO: mark_as_sale
-    println!("Mark as sale: {}", mark_as_sale);
-    task::spawn_blocking(move || {
+    let changes = task::spawn_blocking(move || {
         let rt = tokio::runtime::Runtime::new().unwrap();
         rt.block_on(async move {
             // 1) Connect
@@ -217,11 +216,18 @@ pub async fn remove_stock(changes: Vec<StockChange>, mark_as_sale: bool) -> Resu
             // 4) Commit, note we need to handle potential empty baton error message gracefully
             let commit_res = tx.commit().await;
             ignore_empty_baton_commit(commit_res)?;
-            Ok(())
+            Ok(changes)
         })
     })
     .await
-    .map_err(|e| e.to_string())?
+    .map_err(|e| e.to_string())??;
+
+    if mark_as_sale {
+        // return add_sale(&changes, None).await;
+        return add_sale(&changes, None).await; // changes is still available here
+    } else {
+        Ok(())
+    }
 }
 
 #[tauri::command]
